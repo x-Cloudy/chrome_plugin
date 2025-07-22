e134d82661bd32af6a6abaa6a438ae128d3da5ce
Juan Rodrigues <cloudibr@gmail.com>
2025-07-22 12:56:19 -0300

    CHORE: structure refined

---
+++ public/app/background.js
@@ -0,0 +1,26 @@
+import ChromeMessageHandler from "./modules/ChromeMessageHandler.js";
+
+chrome.runtime.onInstalled.addListener(() => {
+  // chrome.action.disable();
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    const rule = {
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {
+            hostSuffix: "web.whatsapp.com",
+            schemes: ["https"]
+          },
+        }),
+      ],
+      actions: [new chrome.declarativeContent.ShowAction()],
+    };
+
+    chrome.declarativeContent.onPageChanged.addRules([rule]);
+  });
+});
+
+
+
+new ChromeMessageHandler();
+
